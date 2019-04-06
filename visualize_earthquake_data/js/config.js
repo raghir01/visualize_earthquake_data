4c55822aa3f55a9173c139359d7afc146981760c
Raghi Ramachandran <raghir01@gmail.com>
2019-04-06 15:15:20 -0400

    token updated

--- visualize_earthquake_data/js/config.js
+++ visualize_earthquake_data/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+const API_KEY = "pk.eyJ1IjoicmFnaGlyMDEiLCJhIjoiY2p1NXZseTA1MWE1ZzQzbzFnOGRxcXhrYSJ9.V2ejOU0c_IWLpOnDpwvQ1g";
